a13e9b2a0a1cb21ab338be604762ecbd5d1c91ee
Wu Yongzheng <wuyongzheng@gmail.com>
2013-03-12 15:39:47 +0800

    everythin except actual tunnel. not tested

--- natunnel.c
+++ natunnel.c
@@ -9,8 +9,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
 
 #define EXPIRE (4*60)
+#define TIMEOUT 10
+#define PUNCH_RETRY 5
 
 char *option_serverip;
 int option_serverport;
@@ -20,7 +23,7 @@ int option_outport;
 int option_inport;
 char option_role;
 
-int tab_explode (char *str, int argc, char *argv[])
+static int tab_explode (char *str, int argc, char *argv[])
 {
 	int i;
 
@@ -40,7 +43,7 @@ int tab_explode (char *str, int argc, char *argv[])
 	return i;
 }
 
-int resolve_ipv4_address (const char *addrstr, struct sockaddr_in *addr, int port)
+static int resolve_ipv4_address (const char *addrstr, struct sockaddr_in *addr, int port)
 {
 	struct addrinfo hints;
 	struct addrinfo *result, *rp;
@@ -61,34 +64,19 @@ int resolve_ipv4_address (const char *addrstr, struct sockaddr_in *addr, int por
 	return 1;
 }
 
-int run_register (void)
+static int whoami_tcp (struct sockaddr_in *server_addr, int *intport, struct sockaddr_in *extaddr)
 {
-	int sock, msglen;
-	struct sockaddr_in addr;
-	char msg[500];
-
-	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0);
-
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	assert(sock >= 0);
-
-	assert(sendto(sock, "REGISTER", strlen("REGISTER"), 0, (struct sockaddr *)&addr, sizeof(addr)) == strlen("REGISTER"));
-	msglen = recvfrom(sock, msg, sizeof(msg)-1, 0, NULL, NULL);
-	assert(msglen > 0);
-	msg[msglen] = '\0';
-	puts(msg);
-	return 0;
-}
-
-void whoami_tcp (struct sockaddr_in *server_addr, int *intport, struct sockaddr_in *extaddr)
-{
-	int sock, argc, msglen;
+	int sock, argc, msglen, retval = 1;
 	char msg[500], *argv[6];
+	struct timeval tv;
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	assert(sock >= 0);
 	argc = 1;
 	assert(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &argc, sizeof(int)) == 0);
+	tv.tv_sec = TIMEOUT;
+	tv.tv_usec = 0;
+	assert(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval)) == 0);
 
 	if (*intport) {
 		struct sockaddr_in intaddr;
@@ -98,14 +86,23 @@ void whoami_tcp (struct sockaddr_in *server_addr, int *intport, struct sockaddr_
 		assert(bind(sock, (struct sockaddr *)&intaddr, sizeof(intaddr)) == 0);
 	}
 
-	assert(connect(sock, (struct sockaddr *)server_addr, sizeof(struct sockaddr_in)) == 0);
+	if (connect(sock, (struct sockaddr *)server_addr, sizeof(struct sockaddr_in))) {
+		printf("connect to WAI tcp server failed\n");
+		goto out;
+	}
 	msglen = recv(sock, msg, sizeof(msg)-1, 0);
-	assert(msglen > 0);
+	if (msglen <= 0) {
+		printf("recv from WAI tcp server failed\n");
+		goto out;
+	}
 	msg[msglen] = '\0';
 	puts(msg);
 	argc = tab_explode(msg, sizeof(argv)/sizeof(argv[0]), argv);
-	assert(argc == 4);
-	assert(strcmp("WHOYOUARE", argv[0]) == 0);
+	if (argc != 4 || strcmp("WHOYOUARE", argv[0]) ||
+			resolve_ipv4_address(argv[1], extaddr, atoi(argv[2]))) {
+		printf("WAI tcp server response error\n");
+		goto out;
+	}
 
 	if (!*intport) {
 		struct sockaddr_in intaddr;
@@ -114,57 +111,258 @@ void whoami_tcp (struct sockaddr_in *server_addr, int *intport, struct sockaddr_
 		assert(getsockname(sock, (struct sockaddr *)&intaddr, &addrlen) == 0);
 		*intport = ntohs(intaddr.sin_port);
 	}
-	assert(resolve_ipv4_address(argv[1], extaddr, atoi(argv[2])) == 0);
+	retval = 0;
+out:
 	close(sock);
+	return retval;
 }
 
-int do_whoami (int ntlclient, int *intport, struct sockaddr_in *extaddr, int tworound)
+/* return: 0 succeed.
+ *         1 unmatch. (only happens when tworound=1)
+ *         2 other error.
+ * */
+static int do_whoami (int ntlclient, int *intport, struct sockaddr_in *extaddr, int tworound)
 {
 	int msglen, argc;
 	struct sockaddr_in addr, server_addr;
+	struct timeval tv;
 	char msg[500], *argv[6];
 
-	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0); // should we cache?
+	if (send(ntlclient, "WHOAMI", strlen("WHOAMI"), 0) != strlen("WHOAMI")) {
+		printf("send WHOAMI error\n");
+		return 2;
+	}
 
-	assert(sendto(ntlclient, "WHOAMI", strlen("WHOAMI"), 0, (struct sockaddr *)&addr, sizeof(addr)) == strlen("WHOAMI"));
-	msglen = recvfrom(ntlclient, msg, sizeof(msg)-1, 0, NULL, NULL);
+	tv.tv_sec = TIMEOUT;
+	tv.tv_usec = 0;
+	assert(setsockopt(ntlclient, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval)) == 0);
+	msglen = recv(ntlclient, msg, sizeof(msg)-1, 0);
+	if (msglen <= 0) {
+		printf("recv WHOAMI response error\n");
+		return 2;
+	}
+	msg[msglen] = '\0';
+	puts(msg);
+	argc = tab_explode(msg, sizeof(argv)/sizeof(argv[0]), argv);
+	if (argc != 5 || strcmp(argv[0], "WAI_SERVERS") != 0) {
+		printf("WHOAMI response error\n");
+		return 2;
+	}
+
+	if (resolve_ipv4_address(argv[1], &server_addr, atoi(argv[2]))) {
+		printf("WHOAMI response error\n");
+		return 2;
+	}
+	if (whoami_tcp(&server_addr, intport, extaddr))
+		return 2;
+	if (!tworound)
+		return 0;
+
+	if (resolve_ipv4_address(argv[3], &server_addr, atoi(argv[4]))) {
+		printf("WHOAMI response error\n");
+		return 2;
+	}
+	if (whoami_tcp(&server_addr, intport, &addr))
+		return 2;
+	if (memcmp(extaddr, &addr, sizeof(addr)))
+		return 1;
+	return 0;
+}
+
+static int do_update (int ntlclient, const char *ntlid, struct sockaddr_in *extaddr)
+{
+	int msglen;
+	char msg[500];
+	struct timeval tv;
+
+	msglen = sprintf(msg, "UPDATE\t%s\t%s\t%d\tdummy",
+			ntlid, inet_ntoa(extaddr->sin_addr), ntohs(extaddr->sin_port));
+	if (send(ntlclient, msg, msglen, 0) != msglen)
+		return 1;
+
+	tv.tv_sec = TIMEOUT;
+	tv.tv_usec = 0;
+	assert(setsockopt(ntlclient, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval)) == 0);
+	msglen = recv(ntlclient, msg, sizeof(msg)-1, 0);
+	if (msglen <= 0) {
+		printf("recv UPDATE response failed\n");
+		return 1;
+	}
+	msg[msglen] = '\0';
+	puts(msg);
+	if (strcmp("UPDATE_OK", msg)) {
+		printf("UPDATE response error\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+// no need to do clean up because exits anyway.
+static int do_tunnel (int intport, const char *peerip, int peerport)
+{
+	int sock, i;
+	struct sockaddr_in addr;
+	char buff[1600];
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	assert(sock >= 0);
+	i = 1;
+	assert(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(int)) == 0);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(intport);
+	if (bind(sock, (const struct sockaddr *)&addr, sizeof(addr))) {
+		printf("bind to port %d failed\n", intport);
+		return 1;
+	}
+
+	if (resolve_ipv4_address(peerip, &addr, peerport)) {
+		printf("failed to resolve %s\n", peerip);
+		return 1;
+	}
+
+	for (i = 0; i < PUNCH_RETRY; i ++) {
+		if (connect(sock, (const struct sockaddr *)&addr, sizeof(addr)) == 0)
+			break;
+		if (errno != ETIMEDOUT) {
+			perror("connect() failed. retry in 2 sec.");
+			sleep(2);
+		} else {
+			perror("connect() failed.");
+		}
+	}
+
+	if (i >= PUNCH_RETRY) {
+		printf("fails to punch through NAT\n");
+		return 1;
+	}
+
+	snprintf(buff, sizeof(buff), "Hi, I'm %d.", getpid());
+	printf("sending \"%s\"\n", buff);
+	if (send(sock, buff, strlen(buff) + 1, 0) != strlen(buff) + 1) {
+		printf("send() failed.");
+		return 1;
+	}
+	if (recv(sock, buff, sizeof(buff), 0) <= 0) {
+		printf("recv() failed.");
+		return 1;
+	}
+	printf("received \"%s\"\n", buff);
+
+	return 0;
+}
+
+static int run_register (void)
+{
+	int sock, msglen;
+	struct sockaddr_in addr;
+	char msg[500];
+
+	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0);
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	assert(sock >= 0);
+	assert(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+
+	assert(send(sock, "REGISTER", strlen("REGISTER"), 0) == strlen("REGISTER"));
+	msglen = recv(sock, msg, sizeof(msg)-1, 0);
+	assert(msglen > 0);
+	msg[msglen] = '\0';
+	puts(msg);
+	return 0;
+}
+
+static int run_whoami (void)
+{
+	int sock, intport = 0;
+	struct sockaddr_in addr;
+
+	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0);
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	assert(sock >= 0);
+	assert(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+
+	do_whoami(sock, &intport, &addr, 1);
+
+	return 0;
+}
+
+static int run_passive (void)
+{
+	int sock, intport = 0;
+	struct sockaddr_in addr;
+	time_t lastupdate;
+
+	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0);
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	assert(sock >= 0);
+	assert(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+
+	assert(do_whoami(sock, &intport, &addr, 1) == 0);
+	assert(do_update(sock, option_ntlid, &addr) == 0);
+	lastupdate = time(NULL);
+
+	while (1) {
+		struct timeval tv;
+		time_t currtime;
+		char msg[500], *argv[4];
+		int msglen, argc;
+
+		currtime = time(NULL);
+		if (currtime - lastupdate >= EXPIRE) {
+			if (do_whoami(sock, &intport, &addr, 0) == 0) {
+				do_update(sock, option_ntlid, &addr);
+			}
+			lastupdate = currtime; // we ignore update failure.
+		}
+
+		tv.tv_sec = lastupdate + EXPIRE - currtime;
+		tv.tv_usec = 0;
+			assert(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval)) == 0);
+		msglen = recv(sock, msg, sizeof(msg)-1, 0);
+		if (msglen <= 0)
+			continue;
+		msg[msglen] = '\0';
+		puts(msg);
+		argc = tab_explode(msg, sizeof(argv)/sizeof(argv[0]), argv);
+		if (argc != 3 || strcmp(argv[0], "INVITE_P") != 0)
+			continue;
+
+		if (fork() == 0) {
+			close(sock);
+			do_tunnel(intport, argv[1], atoi(argv[2]));
+			exit(0);
+		}
+	}
+
+	return 0;
+}
+
+static int run_active (void)
+{
+	int sock, intport = 0, msglen, argc;
+	struct sockaddr_in addr;
+	char msg[500], *argv[4];
+
+	assert(resolve_ipv4_address(option_serverip, &addr, option_serverport) == 0);
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	assert(sock >= 0);
+	assert(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) == 0);
+	assert(do_whoami(sock, &intport, &addr, 1) == 0);
+
+	msglen = sprintf(msg, "INVITE\t%s\t%s\t%d\tdummy",
+			option_ntlid, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+	assert(send(sock, msg, msglen, 0) == msglen);
+
+	msglen = recv(sock, msg, sizeof(msg)-1, 0);
 	assert(msglen > 0);
 	msg[msglen] = '\0';
 	puts(msg);
 	argc = tab_explode(msg, sizeof(argv)/sizeof(argv[0]), argv);
-	assert(argc == 5);
-
-	assert(resolve_ipv4_address(argv[1], &server_addr, atoi(argv[2])) == 0);
-	whoami_tcp(&server_addr, intport, extaddr);
-	if (!tworound)
-		return 0;
-
-	assert(resolve_ipv4_address(argv[3], &server_addr, atoi(argv[4])) == 0);
-	whoami_tcp(&server_addr, intport, &addr);
-	return memcmp(extaddr, &addr, sizeof(addr));
-}
-
-int run_whoami (void)
-{
-	int sock, intport;
-	struct sockaddr_in extaddr;
-
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	assert(sock >= 0);
-
-	do_whoami(sock, &intport, &extaddr, 1);
-
-	return 0;
-}
-
-int run_passive (void)
-{
-	return 0;
-}
-
-int run_active (void)
-{
-	return 0;
+	assert(argc == 3 && strcmp(argv[0], "INVITE_A") == 0);
+	return do_tunnel(intport, argv[1], atoi(argv[2]));
 }
 
 int main (int argc, char *argv[])
